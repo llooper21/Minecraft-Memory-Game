5d8a598c3e751ff0fb79de7d31cf9bb96477a293
llooper21 <minecraftocelot@icloud.com>
2017-04-18 16:29:49 -1000

    Fixed point system

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var matches = [];
 // startGame();
 
 function flip(clicked_id) {
-	console.log(random);
+	// console.log(random);
 	// console.log(clicked_id);
 	// console.log(newArray);
 	if (random == 1) {
@@ -106,7 +106,6 @@ function flip(clicked_id) {
 			matches += newArray[0];
 			newArray = [];
 		} else {
-
 			document.getElementById('card1').style.backgroundImage = "url('assets/Block.png')";
 			document.getElementById('card2').style.backgroundImage = "url('assets/Block.png')";
 			document.getElementById('card3').style.backgroundImage = "url('assets/Block.png')";
@@ -168,6 +167,7 @@ function flip(clicked_id) {
 					document.getElementById('card8').style.backgroundImage = "url('assets/Cake.png')";
 				}
 			}
+		}
 		if (points == 4) {
 			document.getElementById('score').innerHTML = 'You win!';
 			document.getElementById('score').style.color = "lime";
@@ -184,7 +184,6 @@ function flip(clicked_id) {
 			document.getElementById('score').innerHTML = 'Points = 0';
 			document.getElementById('score').style.color = "black";
 		}
-	}
 	console.log(points);
 	}
 }
